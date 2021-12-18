19e49be877d22f0f659e1577a54c76e5037c0599
LadyKillerKI <kyriejad23@gmail.com>
2021-12-19 00:09:44 +0700

    edit file jupyter

--- PhanLoaiTieuDeBaiBao.py
+++ PhanLoaiTieuDeBaiBao.py
@@ -26,30 +26,30 @@ msg_test, label_test = strat_test_set['title'], strat_test_set['label']
 # msg_train, msg_test, label_train, label_test = train_test_split(df['title'], df['label'], test_size=0.3)
 
 # Training a model
-pipeline = Pipeline([
-    ('bow', CountVectorizer(analyzer=text_process)),
-    ('tfidf', TfidfTransformer()),
-    ('model', OneVsRestClassifier(MultinomialNB()))
-])
-
-pipeline.fit(msg_train, label_train)
-pickle.dump(pipeline, open(r'model_MultiNB.sav', 'wb'))
+# pipeline = Pipeline([
+#     ('bow', CountVectorizer(analyzer=text_process)),
+#     ('tfidf', TfidfTransformer()),
+#     ('model', OneVsRestClassifier(MultinomialNB()))
+# ])
+#
+# pipeline.fit(msg_train, label_train)
+# pickle.dump(pipeline, open(r'model_MultiNB.sav', 'wb'))
 
 pipeline2 = Pipeline([
     ('bow', CountVectorizer(analyzer=text_process)),
     ('tfidf', TfidfTransformer()),
-    ('model', OneVsRestClassifier(SVC()))
+    ('model', OneVsRestClassifier(SVC(kernel='rbf')))
 ])
 pipeline2.fit(msg_train, label_train)
-filename2 = open(r'model_SVC.sav', 'wb')
-pickle.dump(pipeline2, filename2)
-filename2.close()
+# filename2 = open(r'model_SVC.sav', 'wb')
+# pickle.dump(pipeline2, filename2)
+# filename2.close()
 
 # Đánh giá model
-predictions = pipeline.predict(msg_test)
+# predictions = pipeline.predict(msg_test)
 predictions2 = pipeline2.predict(msg_test)
 
-print("Mô hình Muitinomial Naive Bayes: ")
-print(classification_report(label_test, predictions))
+# print("Mô hình Muitinomial Naive Bayes: ")
+# print(classification_report(label_test, predictions))
 print("Mô hình Support vector machine: ")
 print(classification_report(label_test, predictions2))
