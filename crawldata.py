c6a5ab954d72fc04bd508b49c54abaa263628def
maivantien1107 <maivantien1107@users.noreply.github.com>
2022-01-08 17:11:48 +0700

    update file crawl data

--- crawldata.py
+++ crawldata.py
@@ -1,45 +1,52 @@
-list=['so-hoa-p','thoi-su-p','oto-xe-may-p','doi-song-p','du-lich-p','giai-tri-p','giao-duc-p','khoa-hoc-p','kinh-doanh-p','phap-luat-p','suc-khoe-p','the-thao-p','thoi-su-p']
+list=['so-hoa','oto-xe-may','doi-song','du-lich','giai-tri','giao-duc','khoa-hoc','kinh-doanh','phap-luat','suc-khoe','the-thao','thoi-su']
 import requests
 from bs4 import BeautifulSoup
-list1='suc-khoe'
-tmp=("https://vnexpress.net/") + list1+ '-p'
 link_lisk=[]
-data=[]
+for one in list:
+    tmp=("https://vnexpress.net/") + one
+    link_lisk.append(tmp)
 for one in range(1,301):
     item=tmp+str(one)
     link_lisk.append(item)
-dem=0
-check=0
-for one in link_lisk:
-    response=requests.get(one)
-    soup = BeautifulSoup(response.content, "html.parser")
-    if soup.findAll('h3', class_='title-news') is not None:
-        titles = soup.findAll('h3', class_='title-news')
-    if soup.findAll('h2', class_='title-news') is not None:
-        titles = soup.findAll('h2', class_='title-news')
-    links = [link.find('a').attrs["href"] for link in titles]
-    for link in links:
-        news = requests.get(link)
-        soup = BeautifulSoup(news.content, "html.parser")
-        title1 = soup.find("h1", class_="title-detail")
-        if dem==200:
-            check=1
-            break;
-        if title1 is not None:
-            data.append(title1.text)
-            print(title1.text)
-            dem=dem+1
-        else:
-            continue
-    if check==1:
-        print(dem)
-        break
-path_w='data/datatest/' + list1 +'.txt'
-print(path_w)
-with open(path_w, mode='w',encoding = "utf-8") as f:
-    for text in data:
-        f.write(text + '\n')
-f.close()
+for tmp1 in list:
+    check=0
+    dem=0
+    data=[]
+    links=[]
+    for tmp2 in range(1,500):
+        tmp3= ("https://vnexpress.net/") + tmp1+ '-p' + str(tmp2)
+        response=requests.get(tmp3)
+        soup = BeautifulSoup(response.content, "html.parser")
+        if soup.findAll('h3', class_='title-news') is not None:
+            titles = soup.findAll('h3', class_='title-news')
+        else :
+            if soup.findAll('h2', class_='title-news') is not None:
+                titles = soup.findAll('h2', class_='title-news')
+            else :
+                titles = soup.findAll('h1', class_='title-news')
+        links = [link.find('a').attrs["href"] for link in titles]
+        for link in links:
+            news = requests.get(link)
+            soup = BeautifulSoup(news.content, "html.parser")
+            title1 = soup.find("h1", class_="title-detail")
+            if dem==2000:
+                check=1
+                break;
+            if title1 is not None:
+                data.append(title1.text)
+                print(title1.text)
+                dem=dem+1
+            else:
+                continue
+        if check==1:
+            print(dem)
+            break
+    path_w='data/datatest/' + tmp1 +'.txt'
+    print(path_w)
+    with open(path_w, mode='w',encoding = "utf-8") as f:
+        for text in data:
+            f.write(text + '\n')
+    f.close()
 
 
 
