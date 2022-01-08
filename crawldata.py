33af74b6be6559cbcd97dbdb31263498cbe0db7f
maivantien1107 <maivantien1107@users.noreply.github.com>
2022-01-08 16:43:14 +0700

    add data suckhoe

--- crawldata.py
+++ crawldata.py
@@ -1,7 +1,7 @@
 list=['so-hoa-p','thoi-su-p','oto-xe-may-p','doi-song-p','du-lich-p','giai-tri-p','giao-duc-p','khoa-hoc-p','kinh-doanh-p','phap-luat-p','suc-khoe-p','the-thao-p','thoi-su-p']
 import requests
 from bs4 import BeautifulSoup
-list1='so-hoa'
+list1='suc-khoe'
 tmp=("https://vnexpress.net/") + list1+ '-p'
 link_lisk=[]
 data=[]
@@ -13,24 +13,28 @@ check=0
 for one in link_lisk:
     response=requests.get(one)
     soup = BeautifulSoup(response.content, "html.parser")
-    titles = soup.findAll('h2', class_='title-news')
+    if soup.findAll('h3', class_='title-news') is not None:
+        titles = soup.findAll('h3', class_='title-news')
+    if soup.findAll('h2', class_='title-news') is not None:
+        titles = soup.findAll('h2', class_='title-news')
     links = [link.find('a').attrs["href"] for link in titles]
     for link in links:
         news = requests.get(link)
         soup = BeautifulSoup(news.content, "html.parser")
         title1 = soup.find("h1", class_="title-detail")
-        if dem==2000:
+        if dem==200:
             check=1
             break;
         if title1 is not None:
             data.append(title1.text)
+            print(title1.text)
             dem=dem+1
         else:
             continue
     if check==1:
         print(dem)
         break
-path_w='data/' + list1 +'.txt'
+path_w='data/datatest/' + list1 +'.txt'
 print(path_w)
 with open(path_w, mode='w',encoding = "utf-8") as f:
     for text in data:
